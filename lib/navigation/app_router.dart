b94c77a2a79c345f76706033b25b1a76e117b226
thezx <thezxdev@gmail.com>
2022-12-12 17:11:52 -0700

    Creación del app_router

---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,33 @@
+
+
+import 'package:go_router/go_router.dart';
+
+import '../models/models.dart';
+
+class AppRouter {
+
+  final AppStateManager appStateManager;
+  final ProfileManager profileManager;
+  final GroceryManager groceryManager;
+
+  AppRouter(
+    this.appStateManager,
+    this.profileManager,
+    this.groceryManager,
+  );
+
+  late final router = GoRouter(
+    debugLogDiagnostics: true,
+    refreshListenable: appStateManager,
+    initialLocation: '/login',
+    routes: [
+      // TODO: Add Login Route
+      // TODO: Add Onboarding Route
+      // TODO: Add Home Route
+    ],
+    // TODO: Add Error handler
+    // TODO: Add Redirect handler
+    
+  );
+
+}
